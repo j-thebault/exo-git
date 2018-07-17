7080609115c20435dac8738298a8172b03ad033f
Julien Thebault <jthebault@NX-DOMAIN.LAN>
2018-07-17 09:21:58 +0200

    adding src

---
+++ src/main/java/org/afcepf/ai/App.java
@@ -0,0 +1,15 @@
+package org.afcepf.ai;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( StringUtils.reverse("Hello World!"));
+    }
+}
