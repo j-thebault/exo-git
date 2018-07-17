85bbb5e46bb1e2c4ab1fdeb7dfa0723fbb8fd8e4
Julien Thebault <jthebault@NX-DOMAIN.LAN>
2018-07-17 09:53:19 +0200

    Modification depuis notepad amend message

--- src/main/java/org/afcepf/ai/App.java
+++ src/main/java/org/afcepf/ai/App.java
@@ -10,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( StringUtils.reverse("Hello World!"));
+        System.out.println( StringUtils.reverse("Hello World Reset!"));
     }
 }
